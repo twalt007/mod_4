bd72665c27af818c394167ca516df09a79f02127
twalt007 <tatumwalter@gmail.com>
2019-11-05 21:06:45 -0800

    class session over; to be continued

--- 05_file_server_demo/server.js
+++ 05_file_server_demo/server.js
@@ -1,1 +1,32 @@
 // Build Static Webserver Here
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+
+const server = http.createServer((request,response)=> {
+    console.log('Server received a request: ', request.url);
+    let filePath = '.'+request.url;
+
+    if(filePath === './'){
+        filePath += 'index.html';
+    }
+
+    const ext = path.extname(filePath);
+
+    console.Console('File Extension: ', ext);
+
+    const mimeTypes = {
+        '.html': ''
+    }
+
+    console.log('File Path: ', filePath);
+    fs.readFile(filePath, (error, data) => {
+        response.end(data,'utf-8');
+    })
+});
+
+server.listen(3000,()=>{
+    console.log('Server is listening @ 127.0.01:3000')
+});
+
