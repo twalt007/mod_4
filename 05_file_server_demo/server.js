f53a8fa69d87ec543d74b0998b6f467ebaa97eea
twalt007 <tatumwalter@gmail.com>
2019-11-09 12:57:49 -0800

    classwork 11/7/2019

--- 05_file_server_demo/server.js
+++ 05_file_server_demo/server.js
@@ -10,6 +10,17 @@ const server = http.createServer((request,response)=> {
 
     if(filePath === './'){
         filePath += 'index.html';
+    } else if(filePath === './api/user'){
+        const user = {
+            name: 'Kathy Smith',
+            username: 'theRealKathy',
+            email: 'kathy@example.com',
+            phone: '(999)732-1114'
+        }
+
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify(user));
+        return;
     }
 
     const ext = path.extname(filePath);
@@ -17,11 +28,33 @@ const server = http.createServer((request,response)=> {
     console.log('File Extension: ', ext);
 
     const mimeTypes = {
-        '.html': ''
+        '.html': 'text/html',
+        '.css':'text/css',
+        '.png':'image/png',
+        '.js':'text/javascript'
     }
+
+    const type = mimeTypes[ext] || null;
+
+    console.log('File MIME Type: ', type);
 
     console.log('File Path: ', filePath);
     fs.readFile(filePath, (error, data) => {
+        console.log('Read File Error: ', error);
+        if(error || !type){
+            if(error.code === 'ENOENT'){
+                fs.readFile('./404.html',(error,data)=>{
+                    response.writeHead(404, {'Content-Type': 'text/html'});
+                    response.end(data, 'utf-8');
+                });
+                return;
+            };
+            response.writeHead(500, 'Internal Server Error');
+            response.end();
+            return;
+        }
+
+        response.writeHead(200, {'Content-Type': type});
         response.end(data,'utf-8');
     })
 });
